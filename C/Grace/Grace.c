dc0de49ed863f9ae5e3daa409a0adf2dc444d9a2
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-11-04 20:24:21 +0100

    Grace almost done

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-int	main(void)
-{
-	return (0);
-}
+#define NL 10
+#define Q 34
+#define FT(x) int main(){const char s[] = "#include <stdio.h>%c#define NL 10%c#define Q 34%c#define FT(x) int main(){const char s[] = %c%s%c;FILE *f = fopen(%cGrace_kid.c%c, %cw%c);fprintf(f,s,NL,NL,NL,Q,s,Q,Q,Q,Q,Q,NL,NL);fclose(f);return 0;}%cFT(0)%c";FILE *f = fopen("Grace_kid.c", "w");fprintf(f,s,NL,NL,NL,Q,s,Q,Q,Q,Q,Q,NL,NL);fclose(f);return 0;}
+FT(0)
